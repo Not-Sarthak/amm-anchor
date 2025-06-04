5e561e457ce51e3ec3cda88eaf830414ee066a21
Not-Sarthak <notsarthakshah@gmail.com>
2025-06-04 16:11:34 +0530

    feat: add instructions and structures

---
+++ programs/amm/src/lib.rs
@@ -0,0 +1,39 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("CaBQ13oi2enJJGfy3y5PcRdXecM4DVBdtVG6D2dCFQgi");
+
+
+#[program]
+pub mod amm {
+    use super::*;
+
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        fee: u16,
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, &ctx.bumps)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, lp_amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(lp_amount, max_x, max_y)
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, lp_amount: u64, min_x: u64, min_y: u64) -> Result<()> {
+        ctx.accounts.withdraw(lp_amount, min_x, min_y)
+    }
+
+    pub fn swap(ctx: Context<Swap>, is_x: bool, amount_in: u64, min_amount_out: u64) -> Result<()> {
+        ctx.accounts.swap(is_x, amount_in, min_amount_out)
+    }
+}
